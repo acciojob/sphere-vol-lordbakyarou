e22ecfd4d8583b1829656334d82a2ad587a8e43b
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-20 14:29:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+let getRadius = document.getElementById("radius");
+	let getValue = document.getElementById("volume");
+
+	 let getRadiu = getRadius.value;
+  let cal = getRadiu * getRadiu * 3.14;
+
+	 getValue.value = cal;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
