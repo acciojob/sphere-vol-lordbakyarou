d6c89503568c89583ea8c32123d11244c711d501
Mayur Hanwate <70631103+lordbakyarou@users.noreply.github.com>
2023-09-21 12:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 function volume_sphere(event) {
     // Prevent the default form submission behavior
-    event.preventDefault();
+    
 
     let getRadius = document.getElementById("radius");
     let getValue = document.getElementById("volume");
 
     let getButton = document.getElementById("submit");
 
-    getButton.addEventListener("click", () => {
+    
         let getRadiu = getRadius.value;
         let cal = getRadiu * getRadiu * 3.14;
         getValue.value = cal;
-    });
+   
+	event.preventDefault();
 }
 
 window.onload = function() {
